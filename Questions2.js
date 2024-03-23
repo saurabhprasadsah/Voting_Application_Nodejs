b9ecc4db5fe86108521400dd15d415e7d177f9f5
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-23 17:49:26 +0530

    questions

---
+++ Questions2.js
@@ -0,0 +1,19 @@
+
+
+async function getData(){
+    return 1;
+}
+
+console.log("A");
+console.log("B");
+
+setTimeout(()=>console.log("C"), 0);
+
+
+getData().then(() =>console.log("I got the data"));
+
+
+console.log("D");
+console.log("E");
+console.log("F");
+
