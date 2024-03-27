ee273951a51b6a86808c7c2150f0e95908746001
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-27 15:27:34 +0530

    voting

--- Questions2.js
+++ Questions2.js
@@ -5,8 +5,10 @@ async function getData(){
 console.log("A");
 console.log("B");
 
+
 setTimeout(()=>console.log("C"), 0);
 getData().then(() =>console.log("I got the data"));
+
 
 
 console.log("D");
